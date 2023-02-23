516d2b7fa1fbf9e247be086ac2907651748e47b4
Frank Obedi <frankmobedi@gmail.com>
2023-02-23 13:51:55 -0500

    Code upload

---
+++ app.py
@@ -0,0 +1,15 @@
+import weather
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET","POST"])
+def index():
+    data = weather.get_forecast()
+    
+    if data == 404:
+        return render_template('404.html')
+    return render_template('weather.html',weather=data[0],forecast_data=data[1],night=data[2])
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ weather.py
@@ -0,0 +1,71 @@
+import math, time, datetime, requests, os
+from flask import request
+
+# get api key
+key = os.getenv('SECRETE_KEY')
+
+def get_day_time(date_time_txt,just_day=False):
+    date_time = date_time_txt.split(' ')
+    year, mon, day = (int(x) for x in date_time[0].split('-'))
+    date_obj = datetime.date(year,mon,day)
+    day = date_obj.strftime("%A")
+        
+    if just_day:
+        return day
+        
+    time_24hr = date_time[1]
+    t = time.strptime(time_24hr,"%H:%M")
+    time_12hr = time.strftime("%I:%M %p", t)
+    day_time = f"{day}, {time_12hr}"        
+    return day_time
+
+def is_night(hour):
+    if hour >= 6 and hour < 18:
+        return False
+    return True
+
+#648fb20010c74c9f8de201608232202
+
+def get_forecast():
+    # set a default city when website loads
+    city = 'Toronto, Canada'        
+    forecast_data = {}
+
+    if request.method == 'POST':
+        city = request.form.get('city')
+    
+    full_weather = 'http://api.weatherapi.com/v1/forecast.json?key={}&q={}&days=6&aqi=no&alerts=no'
+    forecast = requests.get(full_weather.format('648fb20010c74c9f8de201608232202',city)).json()    
+    
+    
+    #   check for 404 error
+    if forecast.get("error") != None:
+        return 404    
+    else:
+
+        hour = forecast['location']['localtime'].split(' ')[1].split(':')
+        night = is_night(int(hour[0]))
+
+        # single day weather data
+        weather_data = {
+        'city': f"{forecast['location']['name']}, {forecast['location']['country']}",
+        'temp': int(forecast['current']["temp_c"]),
+        'cond': forecast['current']["condition"]['text'],
+        'feel': int(forecast['current']["feelslike_c"]),
+        'icon': forecast['current']["condition"]['icon'],
+        'date': get_day_time(forecast['location']['localtime']),
+        }
+
+        # five day weather data
+        for i in range(1, len(forecast["forecast"]["forecastday"])):
+            day = get_day_time(forecast["forecast"]["forecastday"][i]['date'],just_day=True)
+            min_temp = forecast["forecast"]["forecastday"][i]['day']['mintemp_c']
+            max_temp = forecast["forecast"]["forecastday"][i]['day']['maxtemp_c']
+            icon = forecast["forecast"]["forecastday"][i]['day']['condition']['icon']
+            data = {day: [math.floor(min_temp),math.floor(max_temp),icon]}
+            forecast_data.update(data)
+    print("KEY =", key)
+    return weather_data, forecast_data, night
+
+
+    
