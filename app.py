0b57de7c32839ddd60fdb8c73e6dea5d71a574f2
Frank Obedi <frankmobedi@gmail.com>
2023-03-04 19:34:29 -0500

    updated vercel.json and app.py

--- app.py
+++ app.py
@@ -11,5 +11,3 @@ def index():
         return render_template('404.html')
     return render_template('weather.html',weather=data[0],forecast_data=data[1],night=data[2])
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=8000, debug=True)
